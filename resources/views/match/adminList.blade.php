25fd7d4f5e2adeca6e120d956c8cbe34375cb92d
Martin Púchly <martin.puchly@gmail.com>
2020-08-17 15:43:59 +0200

    side bez dorastu

--- resources/views/match/adminList.blade.php
+++ resources/views/match/adminList.blade.php
@@ -54,10 +54,8 @@
         @endforeach
         </tbody>
 </table>
-{{ ($matchs->count()==0) ? '<span style="color:red">Zatiaľ nie sú pridané žiadne zápasy.</span>' : '' }}
-<hr>
-<h4>Sumár:</h4>
-<span>Počet zápasov: {{ $matchs->count() }}</span>
+<?= ($matchs->count()==0) ? '<span style="color:red">Zatiaľ nie sú pridané žiadne zápasy.</span>' : '' ?>
+
 
 
 <script type="application/javascript">
